adf6a51d5b4ce58e6aac2283f6fd7413810a0bfd
jeny0117 <54958987+jeny0117@users.noreply.github.com>
2019-12-07 14:26:42 -0500

    hi

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
+#import all the necessary packages 
 from flask import Flask, render_template, request
-from overall import remove_url, create_dictionary, clean_tweets, get_sentiment, get_label, auth
 import os
 import tweepy 
 from textblob import TextBlob
 import datetime
 import matplotlib.pyplot as plt 
 
+#import all the necessary functions from overall.py file 
+from overall import remove_url, create_dictionary, clean_tweets, get_sentiment, get_label, auth, bar_plot
+
+#setting the image folder for the barplot to be stored in 
 image_folder = os.path.join('static', 'image')
 
-app1 = Flask(__name__)
-app1.config['upload_folder'] = image_folder
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = image_folder
 
-@app1.route("/", methods=["GET", "POST"])
-@app1.route('/index')
+@app.route("/", methods=["GET", "POST"])
 
-def calculate():
+
+def overall_sentiment_analysis():
     if request.method == "POST":
         api = auth()
         user_input = request.form["firstkeyword"]
@@ -22,11 +26,12 @@ def calculate():
         dictionary_tweets = create_dictionary(public_tweets)
         cleaned_tweets = clean_tweets(dictionary_tweets)
         polarity1 = get_sentiment(cleaned_tweets)[0]
-        subjectivity1= get_sentiment(cleaned_tweets)[1]
+        subjectivity1 = get_sentiment(cleaned_tweets)[1]
         analysis = get_sentiment(cleaned_tweets)
         label1 = get_label(analysis, threshold=0)
-    
-        if  request.form['name'].value == "yes":
+
+        #creating a if statement in case the user wants to analyze another keyword
+        if  request.form['yes'] == "yes":
             user_input2 = request.form["secondkeyword"]
             public_tweets2 = api.search(user_input2, count = 1000)
             dictionary_tweets2 = create_dictionary(public_tweets2)
@@ -35,18 +40,22 @@ def calculate():
             polarity2 = get_sentiment(cleaned_tweets2)[0]
             subjectivity2 = get_sentiment(cleaned_tweets2)[1]
             label2 = get_label(analysis2, threshold=0)
-            full_filename = os.path.join(app.config['upload_folder'], 'barplot.png')
+            bar_plot(polarity1, subjectivity1, polarity2, subjectivity2, user_input, user_input2)
+            full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'barplot.png')
             return render_template(
-                "results1.htm", firstkeyword = user_input, polarity = polarity1, subjectivty = subjectivity1,
-                analysis = analysis, label1= label1, secondkeyword = user_input2 , polarity2 = polarity2, subjectivty2 = subjectivity2,
-                label2 = label2, barplot = full_filename ) 
+                "results1.htm", firstkeyword = user_input, polarity1 = polarity1, subjectivity1 = subjectivity1,
+                label1= label1, secondkeyword = user_input2 , polarity2 = polarity2, subjectivity2 = subjectivity2,
+                label2 = label2, barplot = full_filename) 
 
-        else: 
+        elif request.form['no'] == "no": 
             return render_template(
-                "results1.htm", firstkeyword = user_input, polarity = polarity1, subjectivty = subjectivity1,
-                analysis = analysis) 
+            "results1.htm", firstkeyword = user_input, polarity1 = polarity1, subjectivity1 = subjectivity1,
+            analysis = analysis, label1=label1) 
 
     return render_template("overall.htm", error=None)
+
+def home():
+    return render_template("homepage.html")
 
 if __name__ =='__main__':
     app1.run()
