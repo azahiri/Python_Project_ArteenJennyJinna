7b1bf2edff62f33dffe67aecdb0963fac0567d1a
jeny0117 <54958987+jeny0117@users.noreply.github.com>
2019-12-07 17:30:29 -0500

    hi

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ def overall_sentiment_analysis():
         public_tweets = api.search(user_input, count = 1000)
         dictionary_tweets = create_dictionary(public_tweets)
         cleaned_tweets = clean_tweets(dictionary_tweets)
-        polarity1 = get_sentiment(cleaned_tweets)[0]
-        subjectivity1 = get_sentiment(cleaned_tweets)[1]
+        polarity1 = '{0:.2f}'.format(get_sentiment(cleaned_tweets)[0])
+        subjectivity1 = '{0:.2f}'.format(get_sentiment(cleaned_tweets)[1])
         analysis = get_sentiment(cleaned_tweets)
         label1 = get_label(analysis, threshold=0)
 
@@ -37,25 +37,25 @@ def overall_sentiment_analysis():
             dictionary_tweets2 = create_dictionary(public_tweets2)
             cleaned_tweets2 = clean_tweets(dictionary_tweets2)
             analysis2 = get_sentiment(cleaned_tweets2)
-            polarity2 = get_sentiment(cleaned_tweets2)[0]
-            subjectivity2 = get_sentiment(cleaned_tweets2)[1]
+            polarity2 = '{0:.2f}'.format(get_sentiment(cleaned_tweets2)[0])
+            subjectivity2 = '{0:.2f}'.format(get_sentiment(cleaned_tweets2)[1])
             label2 = get_label(analysis2, threshold=0)
             bar_plot(polarity1, subjectivity1, polarity2, subjectivity2, user_input, user_input2)
             full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'barplot.png')
             return render_template(
-                "results1.htm", firstkeyword = user_input, polarity1 = polarity1, subjectivity1 = subjectivity1,
+                "results1.html", firstkeyword = user_input, polarity1 = polarity1, subjectivity1 = subjectivity1,
                 label1= label1, secondkeyword = user_input2 , polarity2 = polarity2, subjectivity2 = subjectivity2,
                 label2 = label2, barplot = full_filename) 
 
         else: 
             return render_template(
-            "results2.htm", firstkeyword = user_input, polarity1 = polarity1, subjectivity1 = subjectivity1,
+            "results2.html", firstkeyword = user_input, polarity1 = polarity1, subjectivity1 = subjectivity1,
             analysis = analysis, label1=label1) 
 
-    return render_template("overall.htm", error=None)
+    return render_template("overall.html", error=None)
 
 def home():
-    return render_template("homepage.html")
+    return render_template("homepage.html", error=None)
 
 if __name__ =='__main__':
     app1.run()
